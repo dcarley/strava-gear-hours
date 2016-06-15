790449c82d29de37ec44da9963ac080b2da619f1
Dan Carley <dan.carley@gmail.com>
2016-06-15 23:42:59 +0100

    Refactor tests to use gomega's ghttp
    The mock transport was originally copied from the tests in the Strava
    library. In hindsight, using Gomega's ghttp package is simpler. It makes the
    responses and pagination for each test much more explicit.
    
    I'm overriding the Dial functions to point the client at our ghttp server.
    The tests are wrapped with some additional context blocks to give responses
    to specific tests. The diff is easier to read without whitespace changes
    (`-b`).

--- main_test.go
+++ main_test.go
@@ -5,11 +5,11 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
 
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 	"testing"
 	"time"
 
@@ -19,42 +19,6 @@ import (
 func TestStravaGearHours(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "StravaGearHours Suite")
-}
-
-type mockResponseTransport struct {
-	http.Transport
-	content    chan ([]byte)
-	statusCode int
-}
-
-func (t *mockResponseTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	var body []byte
-	select {
-	case c := <-t.content:
-		body = c
-	default:
-		body = []byte(`[]`)
-	}
-
-	resp := &http.Response{
-		Status:     http.StatusText(t.statusCode),
-		StatusCode: t.statusCode,
-		Body:       ioutil.NopCloser(bytes.NewBuffer(body)),
-	}
-
-	return resp, nil
-}
-
-func NewMockClient(content chan ([]byte), statusCode int) *strava.Client {
-	t := &mockResponseTransport{
-		content:    content,
-		statusCode: statusCode,
-	}
-
-	httpClient := &http.Client{Transport: t}
-	c := strava.NewClient("", httpClient)
-
-	return c
 }
 
 // Useful for comparing objects that have been through JSON marshalling
@@ -68,32 +32,30 @@ func ActivityNames(activities []*strava.ActivitySummary) (names []string) {
 	return
 }
 
-// Break an activity slice into a slice of JSON response pages.
-func PaginateResponses(resp []*strava.ActivitySummary, pageSize int) (chan ([]byte), error) {
-	total := len(resp)
-	respChan := make(chan []byte, total)
-
-	for min := 0; min < total; min += pageSize {
-		max := min + pageSize
-		if max >= total {
-			max = total
-		}
-
-		buf, err := json.Marshal(resp[min:max])
-		if err != nil {
-			return respChan, err
-		}
-
-		respChan <- buf
-	}
-
-	return respChan, nil
-}
-
 var _ = Describe("main", func() {
-	var activities []*strava.ActivitySummary
+	var (
+		server     *ghttp.Server
+		client     *strava.Client
+		activities []*strava.ActivitySummary
+	)
 
 	BeforeEach(func() {
+		server = ghttp.NewServer()
+		serverURL, err := url.Parse(server.URL())
+		Expect(err).To(BeNil())
+
+		mockDial := func(network, addr string) (net.Conn, error) {
+			return net.Dial(network, serverURL.Host)
+		}
+
+		httpClient := &http.Client{
+			Transport: &http.Transport{
+				Dial:    mockDial,
+				DialTLS: mockDial,
+			},
+		}
+		client = strava.NewClient("", httpClient)
+
 		activities = []*strava.ActivitySummary{
 			{
 				Name:       "ride 1",
@@ -125,101 +87,144 @@ var _ = Describe("main", func() {
 	})
 
 	Describe("GetBike", func() {
-		var athlete *strava.AthleteDetailed
+		Context("good response from API", func() {
+			var athlete *strava.AthleteDetailed
 
-		BeforeEach(func() {
-			athlete = &strava.AthleteDetailed{
-				Bikes: []*strava.GearSummary{
-					{
-						Id:       "1",
-						Name:     "road bike",
-						Primary:  false,
-						Distance: 100.00,
-					}, {
-						Id:       "2",
-						Name:     "my best bike",
-						Primary:  false,
-						Distance: 200.00,
-					}, {
-						Id:       "3",
-						Name:     "fat bike",
-						Primary:  false,
-						Distance: 300.00,
+			BeforeEach(func() {
+				athlete = &strava.AthleteDetailed{
+					Bikes: []*strava.GearSummary{
+						{
+							Id:       "1",
+							Name:     "road bike",
+							Primary:  false,
+							Distance: 100.00,
+						}, {
+							Id:       "2",
+							Name:     "my best bike",
+							Primary:  false,
+							Distance: 200.00,
+						}, {
+							Id:       "3",
+							Name:     "fat bike",
+							Primary:  false,
+							Distance: 300.00,
+						},
 					},
-				},
-			}
+				}
+
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, athlete),
+					),
+				)
+			})
+
+			It("should return matching bike from athlete", func() {
+				myBike := athlete.Bikes[1]
+				out, err := GetBike(client, myBike.Name)
+				Expect(err).To(BeNil())
+				Expect(out).To(Equal(myBike))
+			})
+
+			It("should return error if unable to find matching bike", func() {
+				out, err := GetBike(client, "garbage")
+				Expect(err).To(MatchError("bike not found: garbage"))
+				Expect(out).To(BeNil())
+			})
 		})
 
-		It("should return matching bike from athlete", func() {
-			buf, err := json.Marshal(athlete)
-			Expect(err).To(BeNil())
+		Context("bad response from API", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete"),
+						ghttp.RespondWith(http.StatusInternalServerError, "error"),
+					),
+				)
+			})
 
-			responses := make(chan []byte, 1)
-			responses <- buf
-			client := NewMockClient(responses, http.StatusOK)
-
-			myBike := athlete.Bikes[1]
-			out, err := GetBike(client, myBike.Name)
-			Expect(err).To(BeNil())
-			Expect(out).To(Equal(myBike))
-		})
-
-		It("should return error if unable to find matching bike", func() {
-			buf, err := json.Marshal(athlete)
-			Expect(err).To(BeNil())
-
-			responses := make(chan []byte, 1)
-			responses <- buf
-			client := NewMockClient(responses, http.StatusOK)
-
-			out, err := GetBike(client, "garbage")
-			Expect(err).To(MatchError("bike not found: garbage"))
-			Expect(out).To(BeNil())
-		})
-
-		It("should return errors from HTTP client", func() {
-			client := NewMockClient(nil, http.StatusInternalServerError)
-
-			out, err := GetBike(client, "garbage")
-			Expect(err).To(MatchError("server error"))
-			Expect(out).To(BeNil())
+			It("should return errors from HTTP client", func() {
+				out, err := GetBike(client, "garbage")
+				Expect(err).To(MatchError("server error"))
+				Expect(out).To(BeNil())
+			})
 		})
 	})
 
 	Describe("GetActivities", func() {
 		const pageSize = 2
 
-		It("should paginate when total is divisible by pageSize", func() {
-			in := activities[:len(activities)-1]
-			Expect(len(in) % pageSize).To(Equal(0))
+		Context("total is divisible by pageSize", func() {
+			BeforeEach(func() {
+				activities = activities[:len(activities)-1]
+				Expect(len(activities) % pageSize).To(Equal(0))
 
-			responses, err := PaginateResponses(in, pageSize)
-			Expect(err).To(BeNil())
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, activities[0:2]),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, activities[2:4]),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, []*strava.ActivitySummary{}),
+					),
+				)
+			})
 
-			client := NewMockClient(responses, http.StatusOK)
-			out, err := GetActivities(client, pageSize)
-			Expect(err).To(BeNil())
-			Expect(ActivityNames(out)).To(Equal(ActivityNames(in)))
+			It("should paginate through all results", func() {
+				out, err := GetActivities(client, pageSize)
+				Expect(err).To(BeNil())
+				Expect(ActivityNames(out)).To(Equal(ActivityNames(activities)))
+			})
 		})
 
-		It("should paginate when total is not divisible by pageSize", func() {
-			in := activities
-			Expect(len(in) % pageSize).To(Equal(1))
+		Context("total is not divisible by pageSize", func() {
+			BeforeEach(func() {
+				Expect(len(activities) % pageSize).To(Equal(1))
 
-			responses, err := PaginateResponses(in, pageSize)
-			Expect(err).To(BeNil())
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, activities[0:2]),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, activities[2:4]),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWithJSONEncoded(http.StatusOK, activities[4:5]),
+					),
+				)
+			})
 
-			client := NewMockClient(responses, http.StatusOK)
-			out, err := GetActivities(client, pageSize)
-			Expect(err).To(BeNil())
-			Expect(ActivityNames(out)).To(Equal(ActivityNames(in)))
+			It("should paginate through all results", func() {
+				out, err := GetActivities(client, pageSize)
+				Expect(err).To(BeNil())
+				Expect(ActivityNames(out)).To(Equal(ActivityNames(activities)))
+			})
 		})
 
-		It("should return errors from HTTP client", func() {
-			client := NewMockClient(nil, http.StatusInternalServerError)
-			out, err := GetActivities(client, pageSize)
-			Expect(err).To(MatchError("server error"))
-			Expect(out).To(BeNil())
+		Context("bad response from API", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v3/athlete/activities"),
+						ghttp.RespondWith(http.StatusInternalServerError, "error"),
+					),
+				)
+			})
+
+			It("should return errors from HTTP client", func() {
+				out, err := GetActivities(client, pageSize)
+				Expect(err).To(MatchError("server error"))
+				Expect(out).To(BeNil())
+			})
 		})
 	})
 
